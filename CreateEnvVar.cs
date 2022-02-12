1e2042a5a503d26537a0bfb0f99306f9f691a7c6
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-02-12 15:16:07 -0600

    basic parsing of teh enviromental variables

--- CreateEnvVar.cs
+++ CreateEnvVar.cs
@@ -1,47 +1,53 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace EnvVarManager {
 public class Program {
 	public static void Main(string[] args) {
 		// Console.WriteLine("Hello World!");
-		bool toDelete = false;
+		// bool toDelete = false;
 
 		// string stdin = Console.ReadLine();
 
 		// Check whether the environment variable exists.
-		string value = Environment.GetEnvironmentVariable("Test1");
+		string PATH_value = Environment.GetEnvironmentVariable("PATH");
+		PATH_value = PATH_value.Replace(";", "\r\n");
+		Console.WriteLine($"PATH_: \r\n {PATH_value}");
+		File.WriteAllText("PATH.txt", PATH_value);
+
 		// If necessary, create it.
-		if (value == null) {
-			Environment.SetEnvironmentVariable("Test1", "Value1");
-			toDelete = false;
+		// if (value == null) {
+		// 	Environment.SetEnvironmentVariable("Test1", "Value1");
+		// 	toDelete = false;
 
-			// Now retrieve it.
-			value = Environment.GetEnvironmentVariable("Test1");
-		}
-		// Display the value.
-		Console.WriteLine($"Test1: {value}\n");
+		// 	// Now retrieve it.
+		// 	value = Environment.GetEnvironmentVariable("Test1");
+		// }
+		// // Display the value.
+		// Console.WriteLine($"Test1: {value}\n");
 
-		// Confirm that the value can only be retrieved from the process
-		// environment block if running on a Windows system.
-		if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
-			Console.WriteLine("Attempting to retrieve Test1 from:");
-			foreach (EnvironmentVariableTarget enumValue in Enum.GetValues(
-						 typeof(EnvironmentVariableTarget))) {
-				value = Environment.GetEnvironmentVariable("Test1", enumValue);
-				Console.WriteLine(
-					$"   {enumValue}: {(value != null ? "found" : "not found")}");
-			}
-			Console.WriteLine();
-		}
+		// // Confirm that the value can only be retrieved from the process
+		// // environment block if running on a Windows system.
+		// if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+		// 	Console.WriteLine("Attempting to retrieve Test1 from:");
+		// 	foreach (EnvironmentVariableTarget enumValue in Enum.GetValues(
+		// 				 typeof(EnvironmentVariableTarget))) {
+		// 		value = Environment.GetEnvironmentVariable("Test1", enumValue);
+		// 		Console.WriteLine(
+		// 			$"   {enumValue}: {(value != null ? "found" : "not
+		// found")}");
+		// 	}
+		// 	Console.WriteLine();
+		// }
 
-		// If we've created it, now delete it.
-		if (toDelete) {
-			Environment.SetEnvironmentVariable("Test1", null);
-			// Confirm the deletion.
-			if (Environment.GetEnvironmentVariable("Test1") == null)
-				Console.WriteLine("Test1 has been deleted.");
-		}
+		// // If we've created it, now delete it.
+		// if (toDelete) {
+		// 	Environment.SetEnvironmentVariable("Test1", null);
+		// 	// Confirm the deletion.
+		// 	if (Environment.GetEnvironmentVariable("Test1") == null)
+		// 		Console.WriteLine("Test1 has been deleted.");
+		// }
 	}
 }
 // The example displays the following output if run on a Windows system:
